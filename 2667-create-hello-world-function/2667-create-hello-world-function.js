8dad8c5a932f0a343d7b97a38d9b8a7c1d490695
Dimas Oktavian <92313885+Dimas-oktavian17@users.noreply.github.com>
2023-12-08 12:55:13 +0700

    Time: 59 ms (6.29%), Space: 41.7 MB (41.55%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+
+    return function (...args) {
+        return args = 'Hello World'
+    };
+
+}
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
